3fe82d51d7b3719e21dd6c38ed9029333fe1dc4a
Ali Rıza Şahin <arsahin.dev@gmail.com>
2022-07-01 22:56:25 +0300

    test(App.test.tsx): assert that the app renders and acts accordingly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it('Renders the component accordingly', () => {
+    render(<App />);
+
+    const form:HTMLFormElement = screen.getByTestId("form");
+    expect(form).toBeInTheDocument();
+    
+    const intInput:HTMLInputElement = screen.getByTestId("input");
+    expect(intInput).toBeInTheDocument();
+    
+    const submitButton:HTMLButtonElement = screen.getByTestId("button");
+    expect(submitButton).toBeInTheDocument();
+
+    const resultSpan:HTMLSpanElement = screen.getByTestId("result");
+    expect(resultSpan).toBeInTheDocument();
+  });
+
+  it("Shows the result", async () => {
+    render(<App />);
+    
+    const intInput:HTMLInputElement = screen.getByTestId("input");
+    const submitButton:HTMLButtonElement = screen.getByTestId("button");
+
+    await userEvent.click(intInput);
+    await userEvent.type(intInput, "12");
+    await userEvent.click(submitButton);
+
+    const resultSpan:HTMLSpanElement = screen.getByText("XII");
+    expect(resultSpan).toBeInTheDocument();
+  })
+
 });
