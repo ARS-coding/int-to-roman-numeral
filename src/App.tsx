8eb4fd245856e2497f432ba192d836f6d086de9f
Ali Rıza Şahin <arsahin.dev@gmail.com>
2022-07-01 22:55:31 +0300

    refactor(App.tsx): get the input value with ref

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,25 @@ import React, { useState ,useRef } from "react";
 import { intToRomanNumeral } from "./utils";
 
 function App() {
-  const romanNumeralResult = useRef<HTMLInputElement>(null);
+  const intInput = useRef<HTMLInputElement>(null);
   const [romanNumeral, setRomanNumeral] = useState("");
 
   function handleFormSubmit(e:React.FormEvent) {
     // don't refresh the page after form submission
     e.preventDefault();
 
-    // get the input value from the form element, if it's truthy and bigger than 0, call the intToRomanNumeral function with it  
-    let intInputValue:number = Number(((e.target as HTMLFormElement)[0] as HTMLInputElement).value);
+    // get the input value, if it's truthy and bigger than 0, call the intToRomanNumeral function with it  
+    let intInputValue:number|undefined = Number(intInput.current?.value);
     if(intInputValue && intInputValue > 0) setRomanNumeral(intToRomanNumeral(intInputValue));
   }
 
   return (
     <main className="App">
-      <form id="form" onSubmit={handleFormSubmit}>
-        <input name="int" type="number" min={1} max={3999} />
-        <input type="submit" />
+      <form id="form" onSubmit={handleFormSubmit} data-testid="form">
+        <input ref={intInput} name="int" type="number" min={1} max={3999} data-testid="input" />
+        <button type="submit" data-testid="button">Convert!</button>
       </form>
-      <span ref={romanNumeralResult} id="roman-numeral-result">{romanNumeral}</span>      
+      <span id="roman-numeral-result" data-testid="result">{romanNumeral}</span>      
     </main>
   );
 };
